680217b4ce334ca4f655ebfa67c8d814d2764adb
aliguvenbas <guvenbas@codeshake.io>
2021-03-03 09:44:14 +0300

    map_ : null value of boxed boolean

--- src/main/java/com/ag/boolean_/TryOuts.java
+++ src/main/java/com/ag/boolean_/TryOuts.java
@@ -1,17 +1,30 @@
 package com.ag.boolean_;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class TryOuts {
 
 	@Test
-	public void shouldReturnFalseIfTextNonSense(){
+	public void shouldReturnFalseIfTextNonSense() {
 		String text = "test";
 
 		Boolean aBoolean = Boolean.valueOf(text);
 
 		assertFalse(aBoolean);
+	}
+
+	@Test
+	public void shouldReturnThrowExceptionIfBoxedBooleanNull() {
+		Boolean b = null;
+
+		assertThrows(NullPointerException.class, () -> {
+			if(b) {
+
+			}
+		});
+
 	}
 }
